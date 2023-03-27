ba5349a2537b8ff0cef11b16142dd36c0316db1d
outtime <i@treeo.cn>
2023-03-27 21:39:29 +0800

    修复时间显示为UTC问题

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -10,9 +10,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Dcat\Admin\Traits\HasDateTimeFormatter;
 
 class BaseModel extends Model
 {
+    use HasDateTimeFormatter;
 
     const STATUS_OPEN = 1; // 状态开启
     const STATUS_CLOSE = 0; // 状态关闭
