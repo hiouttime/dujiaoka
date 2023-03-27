f26e47217af81d6046bfc21c450357e8b385a3a5
outtime <i@treeo.cn>
2023-03-27 12:19:45 +0800

    update process exceptions methods

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,49 +1,49 @@
 <?php
-
 namespace App\Exceptions;
-
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
-class Handler extends ExceptionHandler
-{
-    /**
+class Handler extends ExceptionHandler {
+	/**
      * A list of exception types with their corresponding custom log levels.
      *
      * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
      */
-    protected $levels = [
-        //
-    ];
-
-    /**
+	protected $levels = [
+	        //
+	];
+	/**
      * A list of the exception types that are not reported.
      *
      * @var array<int, class-string<\Throwable>>
      */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
+	protected $dontReport = [
+	        //
+	];
+	/**
      * A list of the inputs that are never flashed to the session on validation exceptions.
      *
      * @var array<int, string>
      */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
-    {
-        $this->renderable(function (AppException $exception, $request) {
-        $layout = dujiaoka_config_get('template', 'layui');
-        $tplPath = $layout . '/errors/error';
-        return response()->view($tplPath, ['title' => __('dujiaoka.error_title'), 'content' => $exception->getMessage(), 'url' => ""], 500);
-        });
-    }
+	protected $dontFlash = [
+	        'current_password',
+	        'password',
+	        'password_confirmation',
+	    ];
+	/**
+      * Render an exception into an HTTP response.
+      *
+      * @param  \Illuminate\Http\Request  $request
+      * @param  \Throwable  $exception
+      * @return \Symfony\Component\HttpFoundation\Response
+      *
+      * @throws \Throwable
+      */
+	public function render($request, Throwable $exception) {
+		if ($exception instanceof AppException) {
+			$layout = dujiaoka_config_get('template', 'layui');
+			$tplPath = $layout . '/errors/error';
+			return view($tplPath, ['title' => __('dujiaoka.error_title'), 'content' => $exception->getMessage(), 'url' => ""]);
+		}
+		return parent::render($request, $exception);
+	}
 }
