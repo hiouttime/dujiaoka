177b247801c2ca10bc8857685cb16650f1bd1f04
outtime <i@treeo.cn>
2024-02-22 19:31:52 +0800

    html5标准优化

--- resources/views/unicorn/layouts/_header.blade.php
+++ resources/views/unicorn/layouts/_header.blade.php
@@ -5,6 +5,7 @@
     <title>{{ isset($page_title) ? $page_title : '' }} | {{ dujiaoka_config_get('title') }}</title>
     <meta name="Keywords" content="{{ dujiaoka_config_get('keywords') }}">
     <meta name="Description" content="{{ dujiaoka_config_get('description')  }}">
+    <meta name=”viewport” content=”width=device-width, initial-scale=1, maximum-scale=1″>
     @if(\request()->getScheme() == "https")
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @endif
